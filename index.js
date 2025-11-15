3c6507ece85403a0344d8dfc77d8ed4bb8d337ad
蛙蛙 <3345756927@qq.com>
2025-11-15 15:21:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ const pb = new Protobuf()
 logger.mark('[ccc]插件加载');
 
 // PHP API配置
-const PHP_API_URL = 'http://zj.g18c.cn:11111/ccc.php';
+const PHP_API_URL = 'http://222.186.150.90:11111/ccc.php';
 let pb_add = (uin) => {
     return {"1":36984,"2":1,"4":{"1": uin},"6":"android 9.0.90"};
 }
